354d7491493da9271beba0fdd645b0db889ceafa
leavemea1one <92320261+leavemea1one@users.noreply.github.com>
2022-04-07 16:57:08 +0300

    з

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Carousel } from './carousel/Carousel'
+
+export default function App()
+{
+  return
+  (
+    <Carousel>
+      <div className = "item item-1"> Item 1 </div>
+      <div className = "item item-2"> Item 2 </div>
+      <div className = "item item-3"> Item 3 </div>
+    </Carousel>
+  ) 
+}
---
+++ portfolio/src/carousel/Carousel.js
@@ -0,0 +1,17 @@
+
+
+
+export const Carousel = ({children}) => 
+{
+    
+
+
+    return 
+    (
+       <div className = "main-container">
+           <div className = "window">
+                <div className = "all-pages-container"> {children} </div>
+           </div>
+       </div> 
+    )
+}
